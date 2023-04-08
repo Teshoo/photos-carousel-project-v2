4f16ab8471aeb5ac2e839566a04ca6042ea5a5c8
Aubin <couscous_tachete@hotmail.fr>
2023-04-08 23:52:51 +0200

    adds left routes for editing pages

--- assets/js/router/index.ts
+++ assets/js/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import TripList from "@/js/pages/TripList.vue";
 import EditTrip from '@/js/pages/edit/EditTrip.vue';
+import EditStage from '@/js/pages/edit/EditStage.vue';
+import EditDay from '@/js/pages/edit/EditDay.vue';
 
 const routes = [
     { 
@@ -12,6 +14,16 @@ const routes = [
         path: '/edit/trip/:tripId',
         name: 'editTrip',
         component: EditTrip,
+    },
+    {
+        path: '/edit/trip/:tripId/stage/:stageId',
+        name: 'editStage',
+        component: EditStage,
+    },
+    {
+        path: '/edit/trip/:tripId/stage/:stageId/day/:dayId',
+        name: 'editDay',
+        component: EditDay,
     },
 ]
 
