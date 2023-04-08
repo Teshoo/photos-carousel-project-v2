e2ff4f96a93ed5a2e2f7b3880b9db93b5d57fa24
Aubin <couscous_tachete@hotmail.fr>
2023-04-08 23:27:49 +0200

    fills types.ts with missing enitites (other than Trip)

--- assets/js/types/types.ts
+++ assets/js/types/types.ts
@@ -1,6 +1,4 @@
-export type { Trip }
-
-import axios from 'axios';
+export type { Trip, TripStage, TripDay, Picture, Hideout, Extra }
 
 interface Trip {
     '@context': string,
@@ -10,4 +8,65 @@ interface Trip {
     name: string,
     tripStages: Array<string>,
     extras: Array<string>,
+}
+
+interface TripStage {
+    '@context': string,
+    '@id': string,
+    '@type': string,
+    id: number,
+    name: string,
+    lat: string,
+    lng: string,
+    Trip: Array<string>,
+    tripDays: Array<string>,
+}
+
+interface TripDay {
+    '@context': string,
+    '@id': string,
+    '@type': string,
+    id: number,
+    name: string,
+    date: Date,
+    startHideout: string,
+    endHideout: string,
+    tripStage: string,
+    pictures: Array<string>,
+}
+
+interface Picture {
+    '@context': string,
+    '@id': string,
+    '@type': string,
+    id: number,
+    name: string,
+    shotTime: Date,
+    lat: string,
+    lng: string,
+    imageName: string,
+    imageSize: number,
+    updatedAt: Date,
+    tripDay: string,
+    extras: Array<string>,
+}
+
+interface Hideout {
+    '@context': string,
+    '@id': string,
+    '@type': string,
+    id: number,
+    name: string,
+    lat: string,
+    lng: string,
+}
+
+interface Extra {
+    '@context': string,
+    '@id': string,
+    '@type': string,
+    id: number,
+    name: string,
+    pictures: Array<string>,
+    trip: string,
 }
