a93f2f5ea72abe0aefc623ea1fb069e3dc8277a6
Teshoo <12759671+Teshoo@users.noreply.github.com>
2024-11-20 18:09:54 +0100

    chore: upgrade to Symfony 7.1 and ApiPlatform 4.0

--- config/bundles.php
+++ config/bundles.php
@@ -14,6 +14,6 @@ return [
     Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
